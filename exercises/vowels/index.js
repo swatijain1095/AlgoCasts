243a9b0140a818464ffb57835fb48dc545bc36c7
Swati Jain <jain.swati71@gmail.com>
2024-04-21 17:49:58 +0200

    Commiting changes for vowels and spiral matrix

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,35 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let i = 0;
+  const vowel = ["a", "e", "i", "o", "u"];
+  const arr = str.toLowerCase().split("");
+  arr.forEach((element) => {
+    if (vowel.includes(element)) {
+      i++;
+    }
+  });
+  return i;
+}
 
 module.exports = vowels;
+
+//alternate mothod
+// function vowels(str) {
+//   let count = 0
+//   const checker = ["a", "e", "i", "o", "u"];
+
+//   for(let char of str.toLowerCase()) {
+//     if(checker.includes(char)) {
+//       count++
+//     }
+//   }
+//   return count
+// }
+
+// alternate solution
+// function vowels(str){
+//   const matches = str.match(/[aeiou]/gi)
+//   return matches ? matches.length : 0
+// }
